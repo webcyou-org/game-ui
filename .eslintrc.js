24313a44f56343fc30c78c0a9d4ea1f292626f42
Daisuke Takayama <takayama_daisuke@cocone.co.jp>
2021-06-07 11:57:16 +0900

    refactor: 💡 setting indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: [],
-  // add your custom rules here
-  rules: {},
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended', 'plugin:nuxt/recommended'],
+    plugins: [],
+    // add your custom rules here
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                tabWidth: 4,
+                printWidth: 120
+            }
+        ],
+        'dot-notation': 'off',
+        'no-console': ['warn', { allow: ['warn', 'error'] }],
+        'comma-dangle': ['error', 'never']
+    }
 }
---
+++ components/zelda/item-acquisition.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul class="itemAcquisitionList">
+        <li>
+            <div class="wrap">
+                <p class="image item"><img src="" width="40" /></p>
+                <p class="name">アイテム名</p>
+            </div>
+        </li>
+    </ul>
+</template>
