af40afbcdb6199fba26560fe5c4e5e555b4edf04
panicdragon <d.takayama.web@gmail.com>
2021-06-14 02:52:07 +0900

    refactor: 💡 router base path

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
     // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
     ssr: false,
-
+    router: {
+        base: '/game-ui/'
+    },
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         title: 'game-ui',
