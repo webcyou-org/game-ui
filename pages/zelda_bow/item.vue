cbc6aa44b34f9f33cb35dd62651899382b47c5ba
panicdragon <d.takayama.web@gmail.com>
2021-06-19 13:47:07 +0900

    feat: 🎸 create item view

--- pages/zelda_bow/item.vue
+++ pages/zelda_bow/item.vue
@@ -3,6 +3,11 @@
         <p class="btn primary" @click.stop="onPushAcquisition">
             <a href="javascript:void(0)">ボタン</a>
         </p>
+        <ul class="list itemViewList">
+            <li v-for="item in itemList" :key="item.id" class="pic item" @click.stop="onClickItem(item)">
+                <img :src="item.image" alt="" />
+            </li>
+        </ul>
         <zelda-item-acquisition-list></zelda-item-acquisition-list>
     </div>
 </template>
@@ -17,6 +22,39 @@ import ZeldaItemAcquisitionList from '~/components/zelda/item-acquisition.vue'
     }
 })
 export default class ZeldaBOWItem extends Vue {
+    itemList = [
+        {
+            id: 1,
+            name: '焼きりんご',
+            image: dataurl.apple
+        },
+        {
+            id: 2,
+            name: '焼きりんご',
+            image: dataurl.apple
+        },
+        {
+            id: 3,
+            name: '焼きりんご',
+            image: dataurl.apple
+        },
+        {
+            id: 4,
+            name: '焼きりんご',
+            image: dataurl.apple
+        },
+        {
+            id: 5,
+            name: '焼きりんご',
+            image: dataurl.apple
+        },
+        {
+            id: 6,
+            name: '焼きりんご',
+            image: dataurl.apple
+        }
+    ]
+
     onPushAcquisition() {
         this.$nuxt.$emit('zeldaItemAcquisition', {
             name: '焼きりんご',
@@ -24,6 +62,20 @@ export default class ZeldaBOWItem extends Vue {
             timeStamp: Date.now(),
             y: 0
         })
+    }
+
+    onClickItem(item) {
+        this.$nuxt.$emit('zeldaItemAcquisition', {
+            name: item.name,
+            image: item.image,
+            timeStamp: Date.now(),
+            y: 0
+        })
+        this.removeItem(item)
+    }
+
+    removeItem(item) {
+        this.itemList.splice(this.itemList.indexOf(item), 1)
     }
 }
 </script>
