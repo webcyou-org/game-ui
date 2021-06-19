51d5d7c1a600ebbfc0e6936fb21d87becee1f706
panicdragon <d.takayama.web@gmail.com>
2021-06-19 15:48:14 +0900

    refactor: 💡 create zeleda get item page bg

--- pages/zelda_bow/item.vue
+++ pages/zelda_bow/item.vue
@@ -4,7 +4,13 @@
             <a href="javascript:void(0)">ボタン</a>
         </p>
         <ul class="list itemViewList">
-            <li v-for="item in itemList" :key="item.id" class="pic item" @click.stop="onClickItem(item)">
+            <li
+                v-for="item in itemList"
+                :key="item.id"
+                class="pic item"
+                :style="item.style"
+                @click.stop="onClickItem(item)"
+            >
                 <img :src="item.image" alt="" />
             </li>
         </ul>
@@ -26,32 +32,38 @@ export default class ZeldaBOWItem extends Vue {
         {
             id: 1,
             name: '焼きりんご',
-            image: dataurl.apple
+            image: dataurl.apple,
+            style: this.getItemRandPosition()
         },
         {
             id: 2,
             name: '焼きりんご',
-            image: dataurl.apple
+            image: dataurl.apple,
+            style: this.getItemRandPosition()
         },
         {
             id: 3,
             name: '焼きりんご',
-            image: dataurl.apple
+            image: dataurl.apple,
+            style: this.getItemRandPosition()
         },
         {
             id: 4,
             name: '焼きりんご',
-            image: dataurl.apple
+            image: dataurl.apple,
+            style: this.getItemRandPosition()
         },
         {
             id: 5,
             name: '焼きりんご',
-            image: dataurl.apple
+            image: dataurl.apple,
+            style: this.getItemRandPosition()
         },
         {
             id: 6,
             name: '焼きりんご',
-            image: dataurl.apple
+            image: dataurl.apple,
+            style: this.getItemRandPosition()
         }
     ]
 
@@ -77,12 +89,31 @@ export default class ZeldaBOWItem extends Vue {
     removeItem(item) {
         this.itemList.splice(this.itemList.indexOf(item), 1)
     }
+
+    getItemRandPosition() {
+        return {
+            left: `${Math.floor(Math.random() * 100)}%`,
+            bottom: `${Math.floor(Math.random() * 100)}%`
+        }
+    }
 }
 </script>
 <!-- eslint-disable -->
 <style scoped lang="scss">
 .container {
     min-height: 100vh;
-    background: #878b3f;
+    background: url("~assets/images/bg_zelda_bow_green.jpg") no-repeat;
+    background-size: cover;
+    background-position: center;
+}
+.list.itemViewList {
+    position: absolute;
+    bottom: 0;
+    left: 10%;
+    width: 80%;
+    height: 45%;
+    & > li {
+        position: absolute;
+    }
 }
 </style>
